8c66895518563f5f3c292c144f5d23a7719c1162
Chris Ward <fuzzyLombax@gmail.com>
2015-01-03 16:21:30 -0800

    Initial model creation.

---
+++ django_poke/pokes/models.py
@@ -0,0 +1,25 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+  """docstring for User"""
+  username = models.CharField(max_length=200)
+
+  def __unicode__(self):
+    return self.username
+
+class Poke(models.Model):
+  """docstring for Poke"""
+  send_user = models.ForeignKey(User, related_name="poker")
+  receive_user = models.ForeignKey(User, related_name="pokee")
+  poke_date = models.DateTimeField('date poked')
+
+  def was_poked_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.poke_date <= now
+
+  was_poked_recently.admin_order_field = 'poke_date'
+  was_poked_recently.boolean = True
+  was_poked_recently.short_description = 'Poked recently?'
